c47875cf6d5b234c6f78dcbe25cdc3fd5dc5ca0c
Zeqiang-Lai <916323301@qq.com>
2020-03-20 22:57:34 +0800

    docs: generate ycc docs files

---
+++ docs/cn.search.min.e86f98339c415033b6030c497cf90e29fe376e0eddafa5477d583a207bd40590.js
@@ -0,0 +1,9 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(e){if(input===document.activeElement){return;}
+const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
+input.focus();e.preventDefault();}
+function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/ycc/flexsearch.min.js');loadScript('/ycc/cn.search-data.min.0d048d96801d230a1dd9bab0f559f27c12d04853b3b5949656d2495976978d4f.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
