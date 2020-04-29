f157012ef654966f7b6a3cc13747ee59ca6d805b
Zeqiang-Lai <916323301@qq.com>
2020-04-29 11:19:44 +0800

    refactor: use new diagnostic construction logic

---
+++ ycc/src/main/java/yan/ycc/api/YCErrors.java
@@ -0,0 +1,9 @@
+package yan.ycc.api;
+
+import yan.common.diagnostic.Errors;
+
+/**
+ * This class is just a simple wrapper for {@link Errors} currently.
+ */
+public class YCErrors extends Errors {
+}
