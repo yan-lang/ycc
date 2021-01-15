ae4864d44777b091dfdcdd251d5c8672f1130ec8
Zeqiang-Lai <916323301@qq.com>
2021-01-15 19:38:45 +0800

    update docs

--- docs/cn.search.min.072d16020586a77b209205d0f3429e63df6eac35d1099740d9f6e3b958d44210.js
+++
@@ -1,9 +0,0 @@
-'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(e){if(input===document.activeElement){return;}
-const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
-input.focus();e.preventDefault();}
-function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/ycc/flexsearch.min.js');loadScript('/ycc/cn.search-data.min.42deb99633bcf810b6ba7e6d50731b6e61a784fb0452d9e6b7a5805250062ca1.js',function(){input.required=false;search();});}
-function search(){while(results.firstChild){results.removeChild(results.firstChild);}
-if(!input.value){return;}
-const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
-function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
--- docs/cn.search.min.856ca94354c4db2cf23cf064a89b77ae882a7d67b8c0079edbd03a353a343240.js
+++
@@ -1,9 +0,0 @@
-'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(e){if(input===document.activeElement){return;}
-const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
-input.focus();e.preventDefault();}
-function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/ycc/flexsearch.min.js');loadScript('/ycc/cn.search-data.min.b4034bb5dbc53dec63a06326c9e07083e2c69f141431fe9fe287f053c9e4c10a.js',function(){input.required=false;search();});}
-function search(){while(results.firstChild){results.removeChild(results.firstChild);}
-if(!input.value){return;}
-const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
-function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
