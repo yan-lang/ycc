fd01ab905d56fb6c17c2e7f16be84f7bf8c299d5
Zeqiang-Lai <916323301@qq.com>
2021-01-15 19:43:14 +0800

    update docs

--- docs/cn.search.min.b21991db637318ffa12aa569d5b157db628bb4cdc072b632b267d96af1a4540d.js
+++ docs/cn.search.min.b21991db637318ffa12aa569d5b157db628bb4cdc072b632b267d96af1a4540d.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/ycc/docs/flexsearch.min.js');loadScript('/ycc/docs/cn.search-data.min.28baf8143b73ef0a6c9d9263181b27133ec91b64ce761f77c8d956625e4b5bef.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/ycc/flexsearch.min.js');loadScript('/ycc/cn.search-data.min.0c52be49f07f212f7962fb5cf84b2a01c864aefe9157878c9770acaec4d7cb79.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
